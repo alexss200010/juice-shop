857cb93723b194411fde1097db3614e4583d6cd3
אלכסנדר שוכמן <AlexanderS3@fnx.co.il>
2023-02-27 15:35:03 +0200

    commit

--- test/api/2faSpec.ts
+++ test/api/2faSpec.ts
@@ -228,7 +228,7 @@ describe('/rest/2fa/status', () => {
 describe('/rest/2fa/setup', () => {
   it('POST should be able to setup 2fa for accounts without 2fa enabled', async () => {
     const email = 'fooooo1@bar.com'
-    const password = '123456'
+    const password = '654321'
 
     const secret = 'ASDVAJSDUASZGDIADBJS'
 
